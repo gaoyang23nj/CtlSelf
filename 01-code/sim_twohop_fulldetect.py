237db4952bf4bc9f24851d3dcfd05d7d56af2443
gaoyang23nj <gaoyang23nj@163.com>
2021-01-02 09:20:19 +0800

    denotation change & python case2
    python case2 needs some corrections

--- 01-code/sim_twohop_fulldetect.py
+++ 01-code/sim_twohop_fulldetect.py
@@ -10,12 +10,15 @@ para_rho = 0.01
 
 para_alpha = 0.9
 # 检查强度 每秒执行多少次检查
-para_Um = 1
+para_Um = 2
 
 event_fromsrc = 'rcv_from_src'
 event_selfish = 'to_be_selfish'
 event_detect = 'detect'
 
+state_without_message = 0
+state_with_message = 1
+state_selfish = 2
 
 # selfish的产生 用接触式模拟
 # src独立于N个nodes，不被感染
@@ -134,8 +137,7 @@ class Sim(object):
             self.list_nextContact.append((self.src_nextContact[i], event_fromsrc, i))
 
         # 进行检查/抽查
-        # 以何种方式执行？ 匀速/Poisson分布/随机？
-        # tmp_print_list_detect = []
+        # 以何种方式执行？ 匀速/
         T_m = 1/para_Um
         tmp = 0
         end_detection = self.total_sim_time
@@ -163,8 +165,8 @@ class Sim(object):
             self.list_nextContact.pop(0)
 
             target_detect = np.random.randint(0, self.N)
-            if self.stateNode[target_detect] == 2:
-                self.stateNode[target_detect] = 0
+            if self.stateNode[target_detect] == state_selfish:
+                self.stateNode[target_detect] = state_without_message
             # self.update_to_detect()
             self.res_detect_ability.append(self.running_time)
             self.res_record.append(self.update_nr_nodes_record_with_time())
@@ -178,8 +180,8 @@ class Sim(object):
             self.running_time = t
             self.list_nextContact.pop(0)
 
-            if self.stateNode[i] != 1:
-                self.stateNode[i] = 1
+            if self.stateNode[i] != state_with_message:
+                self.stateNode[i] = state_with_message
             self.update_to_from_src(i)
             self.res_record.append(self.update_nr_nodes_record_with_time())
         elif self.list_nextContact[0][1] == event_selfish:
@@ -193,8 +195,8 @@ class Sim(object):
             self.list_nextContact.pop(0)
 
             self.update_to_be_selfish(i)
-            if self.stateNode[i] == 1:
-                self.stateNode[i] = 2
+            if self.stateNode[i] == state_with_message:
+                self.stateNode[i] = state_selfish
             self.res_record.append(self.update_nr_nodes_record_with_time())
         else:
             print('Internal Err! -- unkown event time:{} eve_list:{}'.format(self.running_time, self.list_nextContact))
@@ -217,28 +219,17 @@ class Sim(object):
                 break
         self.list_nextContact.insert(loc, (tmp_next_time, event_selfish, i))
 
-    def update_to_detect(self):
-        tmp_next_time = self.get_next_wd(para_Um) + self.running_time
-        # 根据控制策略 停止检测
-        if tmp_next_time > self.t1:
-            return
-        self.detect_nextContact = tmp_next_time
-        loc = 0
-        for loc in range(len(self.list_nextContact)):
-            if self.list_nextContact[loc][0] >= tmp_next_time:
-                break
-        self.list_nextContact.insert(loc, (tmp_next_time, event_detect))
 
-def try_once(t0, t1, is_plot):
+def try_once(is_plot):
     total_time = para_total_time
-    the = Sim(para_N, total_time, t0, t1)
-    p, x, h, i, m = the.get_sim_res()
+    the = Sim(para_N, total_time)
+    p, x, r, i, d = the.get_sim_res()
     cost1, cost2 = the.get_cost()
     print('lost ({}) reward'.format(p))
     print('cost_from_sel:{} cost_from_detect:{}'.format(cost1, cost2))
-    _ = plt.plot(x, h, label="h", color='green', marker='o', markersize=0.5)
-    _ = plt.plot(x, i, label="i", color='blue', marker='o', markersize=0.5)
-    _ = plt.plot(x, m, label="m", color='red', marker='o', markersize=0.5)
+    _ = plt.plot(x, r, label="R", color='green', marker='o', markersize=0.5)
+    _ = plt.plot(x, i, label="I", color='blue', marker='o', markersize=0.5)
+    _ = plt.plot(x, d, label="D", color='red', marker='o', markersize=0.5)
 
     if is_plot:
         # Show the plot
@@ -252,57 +243,59 @@ def try_once(t0, t1, is_plot):
         plt.show()
 
 
-def try_10_times(t0, t1, is_plot):
+def try_10_times(run_times, is_plot):
     total_time = para_total_time
-    run_times = 20
-    t_h = np.zeros((run_times, total_time))
-    t_i = np.zeros((run_times, total_time))
-    t_m = np.zeros((run_times, total_time))
-    t_p = np.zeros(run_times)
-    t_cost1 = np.zeros(run_times)
-    t_cost2 = np.zeros(run_times)
+    # 各种状态
+    multi_times_r = np.zeros((run_times, total_time))
+    multi_times_i = np.zeros((run_times, total_time))
+    multi_times_d = np.zeros((run_times, total_time))
+    # percent of wasted reward
+    multi_times_p = np.zeros(run_times)
+    # cost from D(t); cost from U(t)
+    multi_times_D = np.zeros(run_times)
+    multi_times_U = np.zeros(run_times)
     for k in range(run_times):
         # print('*'*20, k)
-        the = Sim(para_N, total_time, t0, t1)
-        p, x, h, i, m = the.get_sim_res()
-        cost1, cost2 = the.get_cost()
+        the = Sim(para_N, total_time)
+        p, x, r, i, d = the.get_sim_res()
+        cost_of_selfish, cost_of_detection = the.get_cost()
         # print('lost ({}) reward'.format(p))
         # print('cost_from_sel:{} cost_from_detect:{}'.format(cost1, cost2))
-        t_cost1[k] = cost1
-        t_cost2[k] = cost2
-        t_p[k] = p
-        t_h[k, :] = h
-        t_i[k, :] = i
-        t_m[k, :] = m
-    cost1 = np.sum(t_cost1) / run_times
-    cost2 = np.sum(t_cost2) / run_times
-    p = np.sum(t_p) / run_times
-    h = np.sum(t_h, axis=0) / run_times
-    i = np.sum(t_i, axis=0) / run_times
-    m = np.sum(t_m, axis=0) / run_times
-    print('*** avg *** t0:{} t1:{}'.format(t0, t1))
+        multi_times_r[k, :] = r
+        multi_times_i[k, :] = i
+        multi_times_d[k, :] = d
+        multi_times_p[k] = p
+        multi_times_D[k] = cost_of_selfish
+        multi_times_U[k] = cost_of_detection
+
+    r = np.sum(multi_times_r, axis=0) / run_times
+    i = np.sum(multi_times_i, axis=0) / run_times
+    d = np.sum(multi_times_d, axis=0) / run_times
+    cost_of_selfish = np.sum(multi_times_D) / run_times
+    cost_of_detection = np.sum(multi_times_U) / run_times
+    p = np.sum(multi_times_p) / run_times
     print('lost ({}) reward'.format(p))
 
-    total_cost = cost1 * (1-para_alpha) + cost2 * para_alpha
-    print('cost_from_sel:{} cost_from_detect:{} total_cost:{}'.format(cost1, cost2, total_cost))
-    new_cost = cost1/100*0.5 + cost2 * 0.5
+    total_cost = cost_of_selfish * (1-para_alpha) + cost_of_detection * para_alpha
+    print('cost_from_sel:{} cost_from_detect:{} total_cost:{}'.format(cost_of_selfish, cost_of_detection, total_cost))
+    new_cost = cost_of_selfish/100*0.5 + cost_of_detection * 0.5
     print('new_cost:{}'.format(new_cost))
     print('\n')
 
     if is_plot:
-        _ = plt.plot(x, h, label="h", color='green', linestyle='-')
+        _ = plt.plot(x, r, label="r", color='green', linestyle='-')
         _ = plt.plot(x, i, label="i", color='blue', linestyle='-')
-        _ = plt.plot(x, m, label="m", color='red', linestyle='-')
+        _ = plt.plot(x, d, label="d", color='red', linestyle='-')
 
-        draw_bound(total_time)
+        # draw_bound(total_time)
         draw_predict(total_time)
 
         _ = plt.xlabel('total waiting time (games)')
-        _ = plt.ylabel('H, I, M')
+        _ = plt.ylabel('R, I, D')
         plt.legend()
         # Show the plot
         plt.show()
-    return cost1, cost2, total_cost, new_cost
+    return cost_of_selfish, cost_of_detection, total_cost, new_cost
 
 
 def draw_predict(total_time):
@@ -310,87 +303,50 @@ def draw_predict(total_time):
 
     def func_nr_i(t):
         para_ele = -(para_lambda + para_rho) * t
-        para_frac = (para_lambda * para_N) / (para_lambda + para_rho)
-        res = para_frac * (1 - np.math.exp(para_ele))
-        return res
+        para_fraction = (para_lambda * para_N) / (para_lambda + para_rho)
+        result = para_fraction * (1 - np.math.exp(para_ele))
+        return result
 
-    def func_nr_m(t):
-        para_ele = -(para_lambda + para_rho) * t
-        para_frac = (para_lambda * para_N) / (para_lambda + para_rho)
-        para_plus = (para_rho * para_N) / (para_lambda + para_rho)
-        res = - para_N * np.math.exp(- para_lambda * t) + para_frac * np.math.exp(para_ele) + para_plus
-        return res
+    def func_nr_d(t):
+        para_ele1 = -(para_lambda + para_Um/para_N) * t
+        para_ele2 = -(para_lambda + para_rho) * t
+        para_fraction1 = (para_rho * para_lambda * para_N) / ((para_lambda + para_Um/para_N)*(para_Um/para_N - para_rho))
+        para_fraction2 = (para_rho * para_lambda * para_N) / ((para_lambda + para_rho)*(para_Um/para_N - para_rho))
+        para_plus = (para_rho * para_lambda * para_N) / ((para_lambda + para_rho)*(para_lambda + para_Um/para_N))
+        result = - para_fraction1 * np.math.exp(para_ele1) + para_fraction2 * np.math.exp(para_ele2) + para_plus
+        return result
 
     sim_i = np.ones(total_time) * -1
-    sim_m = np.ones(total_time) * -1
+    sim_d = np.ones(total_time) * -1
     for i in range(total_time):
         sim_i[i] = func_nr_i(i)
-        sim_m[i] = func_nr_m(i)
-    _ = plt.plot(x, sim_i, label="predict_i", color='black', linestyle='--')
-    _ = plt.plot(x, sim_m, label="predict_m", color='yellow', linestyle='--')
+        sim_d[i] = func_nr_d(i)
+    _ = plt.plot(x, sim_i, label="predict_I", color='black', linestyle='--')
+    _ = plt.plot(x, sim_d, label="predict_D", color='yellow', linestyle='--')
 
     # 比例
-    prop = np.true_divide(sim_m, sim_i+0.000001)
-    _ = plt.plot(x, prop, label="prop", color='black', linestyle='--')
+    prop = np.true_divide(sim_d, sim_i+0.000001)
+    # _ = plt.plot(x, prop, label="prop", color='black', linestyle='--')
 
 
 def draw_bound(total_time):
     x = np.arange(0, total_time)
-    para_frac = (para_lambda * para_N) / (para_lambda + para_rho)
-    para_plus = (para_rho * para_N) / (para_lambda + para_rho)
-    pred_i = np.ones(total_time) * para_frac
-    pred_m = np.ones(total_time) * para_plus
-    _ = plt.plot(x, pred_i, label="bound_i", color='blue', linestyle='--')
-    _ = plt.plot(x, pred_m, label="bound_m", color='red', linestyle='--')
-
-
-def draw_all_combine(total_time, delta):
-    list_t0_t1 = []
-    for i in range(0, total_time+1, delta):
-        for j in range(i+delta, total_time+1, delta):
-            list_t0_t1.append((i, j))
-    print(list_t0_t1)
-    # 保存结果
-    res = []
-    for (t0, t1) in list_t0_t1:
-        c1, c2, tc, nc = try_10_times(t0, t1, False)
-        ele = (t0, t1, c1, c2, tc, nc, total_time, delta)
-        res.append(ele)
-    return res
-
-
-def print_file(res, short_time):
-    file_object = open('result_'+short_time+'_uniform_detect_M_N_U.tmp', 'a+', encoding="utf-8")
-    for ele in res:
-        tmp_string = ''
-        for i in range(len(ele)):
-            tmp_string = tmp_string + str(ele[i])
-            if i != len(ele)-1:
-                tmp_string = tmp_string + ','
-        file_object.write(tmp_string + '\n')
-    file_object.close()
+    para_i = (para_lambda * para_N) / (para_lambda + para_rho)
+    para_d = (para_rho * para_N) / (para_lambda + para_rho)
+    bound_i = np.ones(total_time) * para_i
+    bound_d = np.ones(total_time) * para_d
+    _ = plt.plot(x, bound_i, label="bound_i", color='blue', linestyle='--')
+    _ = plt.plot(x, bound_d, label="bound_m", color='red', linestyle='--')
 
 
 if __name__ == "__main__":
     t1 = datetime.datetime.now()
-    res = draw_all_combine(para_total_time, 10)
 
-    # try_once(0, 5000, True)
-    # 优化结果
-    o_t0 = 108+1
-    o_t1 = 451
-    c1, c2, tc, nc = try_10_times(o_t0, o_t1, False)
-    ele = (o_t0, o_t1, c1, c2, tc, nc, para_total_time, 0)
-    res.append(ele)
+    # try_once(True)
 
-    short_time = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-    print_file(res, short_time+'_time_seq')
+    # # 优化结果
+    try_10_times(20, True)
 
-    def take5(elem):
-        return elem[4]
-
-    res.sort(key=take5)
-    print_file(res, short_time+'_cost_seq')
     t2 = datetime.datetime.now()
     print(t2-t1)
 
