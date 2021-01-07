51738ff0f53b78e570ba62ca444dfa21ef5aad6e
qq2330 <36947687+qq2330@users.noreply.github.com>
2021-01-07 16:47:23 +0800

    update pe by yan

--- 01-code/plot_opt_solve.m
+++ 01-code/plot_opt_solve.m
@@ -7,12 +7,17 @@ re_1=zeros(1,(Len_y-1)/20);
 re_2=zeros(1,(Len_y-1)/20);
 re_3=zeros(1,(Len_y-1)/20);
 re_4=zeros(1,(Len_y-1)/20);
+re_5=zeros(1,Len_y);
 for i=0:1:(Len_y-1)/20
     re_1(1,i+1)=a(i*20+1,1);
     re_2(1,i+1)=a(i*20+1,2);
     re_3(1,i+1)=a(i*20+1,3);
     re_4(1,i+1)=a(i*20+1,4);
 end
+for i=1:1:Len_y
+    re_5(1,i)=a(i,5);
+end
+    
 h=0:20:500;
 figure(1);
 plot(h,re_1,'--','LineWidth',1);
@@ -25,3 +30,11 @@ plot(h,re_2,'-*','LineWidth',1);
 plot(h,re_3,'-^','LineWidth',1);
 plot(h,re_4,'-o','LineWidth',1);
 legend('re_1','re_2','re_3','re_4','re_5','re_6');
+figure(2);
+h2=0:1:500;
+plot(h2,re_5,'-','LineWidth',1);
+axis([0 500 0 1.2]);
+grid on;
+hold on;
+ylabel('Detection Rate','FontSize',12)
+xlabel('Time (s)','FontSize',12)
